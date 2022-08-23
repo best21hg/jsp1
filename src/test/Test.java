41b8d0b39d73d30630d5c057b9a963429b86e59c
simsilbia <tlatlfqldkss@gmail.com>
2022-08-23 16:19:43 +0900

    테스트입니다

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("깃허브 테스트입니다");
+		System.out.println("깃허브 테스트입니다");
+		System.out.println("깃허브 테스트입니다");
+		System.out.println("깃허브 테스트입니다");
+		System.out.println("깃허브 테스트입니다");
+	}
+
+}
